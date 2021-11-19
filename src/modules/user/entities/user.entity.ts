cf02c4ef84ceae4a2386c1e75e2c8ba7a404c1a1
cheymos <nevesomov@gmail.com>
2021-11-19 22:11:13 +0600

    feat(user): implement basic user module
    Created:
    - user service with create and find methods;
    - user entity.

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  constructor(username: string, email: string, password: string) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @Column({ type: 'varchar', length: 36, unique: true })
+  username: string;
+
+  @Column({ type: 'varchar', length: 254, unique: true })
+  email: string;
+
+  @Column('text', { select: false })
+  password: string;
+}
