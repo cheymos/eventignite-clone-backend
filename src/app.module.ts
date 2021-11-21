5a7d656be15d2659b335ec32ddf4f77cbe9149f7
cheymos <nevesomov@gmail.com>
2021-11-21 16:12:52 +0600

    chore: create basic clean event module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getOrmConfig } from './configs/orm.config';
 import { AuthModule } from './modules/auth/auth.module';
+import { EventModule } from './modules/event/event.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
@@ -11,6 +12,7 @@ import { UserModule } from './modules/user/user.module';
     TypeOrmModule.forRoot(getOrmConfig()),
     UserModule,
     AuthModule,
+    EventModule,
   ],
 })
 export class AppModule {}
