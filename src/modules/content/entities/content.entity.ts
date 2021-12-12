fa40fffde0b8dcbaa0aff63af732d30342b18542
cheymos <nevesomov@gmail.com>
2021-12-12 15:25:27 +0600

    refactor(content)!: remove body property form content entity

--- src/modules/content/entities/content.entity.ts
+++ src/modules/content/entities/content.entity.ts
@@ -19,10 +19,6 @@ export class ContentEntity {
   @Column({ type: 'enum', enum: ContentType })
   type: ContentType;
 
-  @ApiProperty()
-  @Column('text')
-  body: string;
-
   @ManyToOne(() => UserEntity, { onDelete: 'CASCADE' })
   owner?: UserEntity;
 
@@ -30,9 +26,8 @@ export class ContentEntity {
   @Column()
   ownerId: number;
 
-  constructor(type: ContentType, body: string, ownerId: number) {
+  constructor(type: ContentType, ownerId: number) {
     this.type = type;
-    this.body = body;
     this.ownerId = ownerId;
   }
 }
