7bcd785747798540645057213ed6b6cbc7efc1a5
cheymos <nevesomov@gmail.com>
2021-12-12 15:28:25 +0600

    feat(content): create new content-variant entity

---
+++ src/modules/content/entities/content-variant.entity.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ContentEntity } from './content.entity';
+
+export enum ContentType {
+  Video = 'video',
+  Audio = 'audio',
+  Picture = 'picture',
+  Html = 'html',
+}
+
+@Entity('content_variants')
+export class ContentVariantEntity {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @ApiProperty()
+  @Column('text')
+  body: string;
+
+  @ManyToOne(() => ContentEntity, { onDelete: 'CASCADE' })
+  content?: ContentEntity;
+
+  @ApiProperty()
+  @Column()
+  contentId: number;
+
+  constructor(body: string, ownerId: number) {
+    this.body = body;
+    this.contentId = ownerId;
+  }
+}
