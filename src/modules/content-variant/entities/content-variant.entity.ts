b5e4fd48f78cf5e0bc01ab252bec08c2ccfe8961
cheymos <nevesomov@gmail.com>
2021-12-17 19:23:03 +0600

    refactor: move content-variant entity to content-variant module

--- src/modules/content-variant/entities/content-variant.entity.ts
+++ src/modules/content-variant/entities/content-variant.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { ContentEntity } from './content.entity';
+import { ContentEntity } from '../../content/entities/content.entity';
 
 export enum ContentType {
   Video = 'video',
