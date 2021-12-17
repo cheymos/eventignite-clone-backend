0a5ec447382a0958471b9fe2b3cc804ad840c999
cheymos <nevesomov@gmail.com>
2021-12-18 02:24:05 +0600

    feat(content-property): create basic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getOrmConfig } from './configs/orm.config';
 import { AuthModule } from './modules/auth/auth.module';
-import { EventModule } from './modules/event/event.module';
-import { UserModule } from './modules/user/user.module';
-import { ScreenModule } from './modules/screen/screen.module';
-import { PlaylistModule } from './modules/playlist/playlist.module';
-import { ContentModule } from './modules/content/content.module';
+import { ContentPropertyModule } from './modules/content-property/content-property.module';
 import { ContentVariantModule } from './modules/content-variant/content-variant.module';
+import { ContentModule } from './modules/content/content.module';
+import { EventModule } from './modules/event/event.module';
+import { PlaylistModule } from './modules/playlist/playlist.module';
+import { ScreenModule } from './modules/screen/screen.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { ContentVariantModule } from './modules/content-variant/content-variant.
     PlaylistModule,
     ContentModule,
     ContentVariantModule,
+    ContentPropertyModule,
   ],
 })
 export class AppModule {}
