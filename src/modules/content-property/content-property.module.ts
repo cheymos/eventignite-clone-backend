2e3bfd59f695ec00e7bd61c7401ce0e1cd2cafd2
cheymos <nevesomov@gmail.com>
2021-12-18 03:13:05 +0600

    fea(content-property): implement the ability to create content property

--- src/modules/content-property/content-property.module.ts
+++ src/modules/content-property/content-property.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContentModule } from '../content/content.module';
 import { ContentPropertyController } from './content-property.controller';
 import { ContentPropertyService } from './content-property.service';
 import { ContentPropertyEntity } from './entities/content-property.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ContentPropertyEntity])],
+  imports: [TypeOrmModule.forFeature([ContentPropertyEntity]), ContentModule],
   controllers: [ContentPropertyController],
   providers: [ContentPropertyService],
 })
