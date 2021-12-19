a34ac025c3ffbb8f000e4ff9a9eb6666a3a1ced0
cheymos <nevesomov@gmail.com>
2021-12-19 15:03:14 +0600

    docs(content-variant): add swagger decorators

--- src/modules/content-variant/content-variant.controller.ts
+++ src/modules/content-variant/content-variant.controller.ts
@@ -11,8 +11,10 @@ import {
   Put,
   UseGuards
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiExtraModels, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatedResponse } from '../../common/types/created-response.type';
 import { PaginateResponse } from '../../common/types/paginate-response.type';
+import { getPaginateResponseOptions } from '../../utils/get-paginate-response-options.util';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { User } from '../user/decorators/user.decorator';
 import { ContentVariantService } from './content-variant.service';
@@ -20,10 +22,19 @@ import { ContentVariantDto } from './dtos/content-variant.dto';
 import { ContentVariantEntity } from './entities/content-variant.entity';
 
 @Controller('contents/:contentId/variants')
+@ApiTags('Content variants')
 @UseGuards(AuthGuard)
 export class ContentVariantController {
   constructor(private readonly contentVariantService: ContentVariantService) {}
 
+  @ApiOperation({ summary: 'Create content variant' })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    type: CreatedResponse,
+    description: 'Successfully created',
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Post()
   async createContentVariant(
     @Body() contentVariantDto: ContentVariantDto,
@@ -39,6 +50,15 @@ export class ContentVariantController {
     return { id };
   }
 
+  @ApiOperation({ summary: 'Get content variant by id' })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    type: ContentVariantEntity,
+    description: 'Successful operation',
+  })
+  @ApiResponse({ status: 403, description: 'Access denied' })
+  @ApiResponse({ status: 404, description: 'Content variant not found' })
   @Get(':id')
   async getContentVariant(
     @Param('id', ParseIntPipe) contentVariantId: number,
@@ -52,6 +72,11 @@ export class ContentVariantController {
     );
   }
 
+  @ApiOperation({ summary: 'Get all content variants' })
+  @ApiBearerAuth()
+  @ApiExtraModels(ContentVariantEntity)
+  @ApiResponse(getPaginateResponseOptions(ContentVariantEntity))
+  @ApiResponse({ status: 403, description: 'Access denied' })
   @Get()
   async getAllContentVariants(
     @Param('contentId', ParseIntPipe) contentId: number,
@@ -60,6 +85,11 @@ export class ContentVariantController {
     return this.contentVariantService.getAllVariants(contentId, userId);
   }
 
+  @ApiOperation({ summary: 'Update content variant by id' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 204, description: 'Successfully updated' })
+  @ApiResponse({ status: 403, description: 'Access denied' })
+  @ApiResponse({ status: 404, description: 'Content variant not found' })
   @Put(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateContentVariant(
@@ -76,6 +106,11 @@ export class ContentVariantController {
     );
   }
 
+  @ApiOperation({ summary: 'Delete content variant by id' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 204, description: 'Successfully deleted' })
+  @ApiResponse({ status: 403, description: 'Access denied' })
+  @ApiResponse({ status: 404, description: 'Content variant not found' })
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteContentVariant(
