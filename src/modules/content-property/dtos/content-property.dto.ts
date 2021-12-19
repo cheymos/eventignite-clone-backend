2c021cf9eafc9ee287967e308100fb67bf7ed7cf
cheymos <nevesomov@gmail.com>
2021-12-19 15:02:50 +0600

    docs(content-property): add swagger decorators

--- src/modules/content-property/dtos/content-property.dto.ts
+++ src/modules/content-property/dtos/content-property.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class ContentPropertyDto {
+  @ApiProperty()
   @IsString()
   property: string;
 
+  @ApiProperty()
   @IsString()
   value: string;
 }
