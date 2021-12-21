bb9435d884224cd5cf0138676205c5e8ecb9ad23
cheymos <nevesomov@gmail.com>
2021-12-21 15:36:14 +0600

    feat(auth): create jwt strategy for auth0

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,21 +1,24 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RefreshTokenEntity } from './entities/refresh-token.entity';
 import { AuthMiddleware } from './middlewares/auth.middleware';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { TokenService } from './token.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RefreshTokenEntity]),
     ConfigModule,
-    UserModule
+    UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, TokenService],
+  providers: [AuthService, TokenService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {
