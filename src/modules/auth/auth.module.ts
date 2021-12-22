4e4ddaaec4a1c69f8e7799a1a64ab28206f585ac
cheymos <nevesomov@gmail.com>
2021-12-22 14:27:23 +0600

    chore: remove unused functionality

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,28 +1,15 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { RefreshTokenEntity } from './entities/refresh-token.entity';
-import { AuthMiddleware } from './middlewares/auth.middleware';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { TokenService } from './token.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RefreshTokenEntity]),
     ConfigModule,
     UserModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
-  controllers: [AuthController],
-  providers: [AuthService, TokenService, JwtStrategy],
-  exports: [AuthService],
+  providers: [JwtStrategy],
 })
-export class AuthModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('*');
-  }
-}
+export class AuthModule {}
