9300b7d882bf66b2c06dc3637bc358495b407294
cheymos <nevesomov@gmail.com>
2021-12-28 16:53:27 +0600

    refactor(screen): change default repository to custom in service

--- src/modules/screen/screen.service.ts
+++ src/modules/screen/screen.service.ts
@@ -1,15 +1,11 @@
 import {
   ForbiddenException,
   Injectable,
-  NotFoundException,
   UnprocessableEntityException
 } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import {
   NO_ACCESS_SCREEN,
-  PLAYLIST_IS_USED,
-  SCREEN_NOT_FOUND
+  PLAYLIST_IS_USED
 } from '../../common/constants/error.constants';
 import { EventRepository } from '../event/event.repository';
 import { EventService } from '../event/event.service';
@@ -17,12 +13,12 @@ import { PlaylistRepository } from '../playlist/repositories/playlist.repository
 import { PlaylistService } from '../playlist/services/playlist.service';
 import { ScreenDto } from './dtos/screen.dto';
 import { ScreenEntity } from './entities/screen.entity';
+import { ScreenRepository } from './screen.repository';
 
 @Injectable()
 export class ScreenService {
   constructor(
-    @InjectRepository(ScreenEntity)
-    private readonly screenRepository: Repository<ScreenEntity>,
+    private readonly screenRepository: ScreenRepository,
     private readonly eventService: EventService,
     private readonly eventRepository: EventRepository,
     private readonly playlstService: PlaylistService,
@@ -59,7 +55,7 @@ export class ScreenService {
   }
 
   async getOne(screenId: number, userId: string): Promise<ScreenEntity> {
-    const screen = await this.findOne(screenId);
+    const screen = await this.screenRepository.findOneById(screenId);
     this.checkAccess(screen, userId);
 
     return screen;
@@ -71,7 +67,7 @@ export class ScreenService {
     userId: string,
   ): Promise<void> {
     try {
-      const screen = await this.findOne(screenId);
+      const screen = await this.screenRepository.findOneById(screenId);
       this.checkAccess(screen, userId);
 
       const event = await this.eventRepository.findOneById(eventId);
@@ -97,7 +93,7 @@ export class ScreenService {
   }
 
   async delete(screenId: number, userId: string): Promise<void> {
-    const screen = await this.findOne(screenId);
+    const screen = await this.screenRepository.findOneById(screenId);
     this.checkAccess(screen, userId);
 
     await this.screenRepository.delete({ id: screenId });
@@ -107,13 +103,5 @@ export class ScreenService {
     const isAllow = screen.ownerId === userId;
 
     if (!isAllow) throw new ForbiddenException(NO_ACCESS_SCREEN);
-  }
-
-  async findOne(screenId: number): Promise<ScreenEntity> {
-    const screen = await this.screenRepository.findOne(screenId);
-
-    if (!screen) throw new NotFoundException(SCREEN_NOT_FOUND);
-
-    return screen;
   }
 }
