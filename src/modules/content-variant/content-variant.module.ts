732824d78bfa5742c96d87b71a90c656f8761ad5
cheymos <nevesomov@gmail.com>
2021-12-28 16:25:59 +0600

    feat(content-variant): create custom repository

--- src/modules/content-variant/content-variant.module.ts
+++ src/modules/content-variant/content-variant.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContentModule } from '../content/content.module';
 import { FileModule } from '../file/file.module';
 import { ContentVariantController } from './content-variant.controller';
+import { ContentVariantRepository } from './content-variant.repository';
 import { ContentVariantService } from './content-variant.service';
 import { ContentVariantEntity } from './entities/content-variant.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ContentVariantEntity]),
+    TypeOrmModule.forFeature([ContentVariantEntity, ContentVariantRepository]),
     ContentModule,
     FileModule,
   ],
